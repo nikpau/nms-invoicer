0605d651f8f8ed025508cf8c053ed9f622c7dac8
Nick Lehmann <nick@lehmann.sh>
2022-05-08 16:38:43 +0200

    Add invoice model

--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
+from dataclasses_json import dataclass_json
 
 # Unique name for an event
 EventSlug = str
@@ -10,3 +11,13 @@ class Event:
   slug: EventSlug
   date: datetime
   location: str
+  
+  
+@dataclass_json
+@dataclass
+class Invoice:
+    event_slug: EventSlug
+    date: datetime
+    purpose: str
+    cost: float
+    user: str
